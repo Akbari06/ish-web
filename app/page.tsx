873a4f6c7acdac57665df43da900e1a53f22c575
Akbari06 <akbarishtiaque@gmail.com>
2026-04-30 22:57:08 +0100

    liquid glass

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,17 @@ export default function Home() {
 
   return (
     <>
+      <svg className="hidden">
+        <defs>
+          <filter id="glass-filter" x="0%" y="0%" width="100%" height="100%" colorInterpolationFilters="sRGB">
+            <feTurbulence type="fractalNoise" baseFrequency="0.05 0.05" numOctaves="1" seed="1" result="turbulence" />
+            <feGaussianBlur in="turbulence" stdDeviation="2" result="blurredNoise" />
+            <feDisplacementMap in="SourceGraphic" in2="blurredNoise" scale="40" xChannelSelector="R" yChannelSelector="B" result="displaced" />
+            <feGaussianBlur in="displaced" stdDeviation="2" result="finalBlur" />
+            <feComposite in="finalBlur" in2="finalBlur" operator="over" />
+          </filter>
+        </defs>
+      </svg>
       <div style={{ position: 'fixed', inset: 0, zIndex: -1, opacity: 0.35, pointerEvents: 'none' }}>
         <AnoAI />
       </div>
